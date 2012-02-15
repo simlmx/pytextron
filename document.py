3c02038b034f2bb1de931ba3536fe9488fdf8896
Simon Lemieux <lemieux.simon@gmail.com>
2012-02-15 10:33:30 -0500

    added a test

--- document.py
+++ document.py
@@ -1,4 +1,4 @@
-import sys, subprocess, re
+import sys, subprocess, re, os
 from os.path import join, exists, dirname
 
 class LatexDocument(object):
@@ -9,15 +9,14 @@ class LatexDocument(object):
         self.preambule = preambule
         self.core = core
 
-    def make_tex(self, tex_file, ask_overwrite=True):
+    def make_tex(self, tex_file):
 
         if exists(tex_file):
-            if ask_overwrite:
-                choice = ''
-                while choice not in ['Y', 'N']:
-                    choice = raw_input("The file %s already exists. Overwrite? (Y/N) " % tex_file).upper()
-                if choice == 'N':
-                    sys.exit()
+            choice = ''
+            while choice not in ['Y', 'N']:
+                choice = raw_input("The file %s already exists. Overwrite? (Y/N) " % tex_file).upper()
+            if choice == 'N':
+                sys.exit()
 
         with open(tex_file, 'w') as f:
             f.write(unicode(self.preambule))
@@ -34,13 +33,15 @@ class LatexDocument(object):
                 '--file-line-error',
                 '--shell-escape',
                 '--synctex=1',
-                os.path.basename(path)], cwd=cwd).communicate()
+                os.path.basename(tex_file)], cwd=cwd).communicate()
 
         # cleaning
         for ext in 'sol qsl synctex.gz log aux'.split():
-            os.remove('%s.%s' % (path[:-4],ext))
+            path = '%s.%s' % (tex_file[:-4], ext)
+            if exists(path):
+                os.remove(path)
         for ext in r'func\d?.gnuplot func\d?.table'.split():
-            regex = re.compile('^%s.%s$' % (path[:-4], ext))
+            regex = re.compile('^%s.%s$' % (tex_file[:-4], ext))
             for f in os.listdir(cwd):
                 if regex.match(f):
                     os.remove(f)
--- tests/test_document.py
+++ tests/test_document.py
@@ -1,5 +1,5 @@
-import unittest
-from os.path import dirname, join
+import unittest, os
+from os.path import dirname, join, exists
 from pytextron.blocks.base import *
 from pytextron.blocks.basic import *
 from pytextron.document import *
@@ -11,13 +11,30 @@ class TestLatexDocument(unittest.TestCase):
         d = Document('patate')
 
         doc = LatexDocument(preambule, d)
-        tex_file = join(dirname(__file__), 'test.tex') 
-        doc.make_tex(tex_file, ask_overwrite=False)
-        
-        self.assertEqual(open(tex_file).read(), 
-            ur'\documentclass{article}' '\n\n'
-            ur'\begin{document}' '\n\tpatate\n'
-            ur'\end{document}')
+        cur_dir = dirname(__file__)
+        tex_file = join(cur_dir, 'test.tex')
+        pdf_file = tex_file[:-3] + 'pdf'
+        if exists(tex_file):
+            os.remove(tex_file)
+        doc.make_tex(tex_file)
+
+        def test_tex_file():
+            self.assertEqual(open(tex_file).read(), 
+                ur'\documentclass{article}' '\n\n'
+                ur'\begin{document}' '\n\tpatate\n'
+                ur'\end{document}')
+        test_tex_file()
+
+        doc.make_pdf(tex_file)
+        self.assertTrue(exists(pdf_file))
+        os.remove(tex_file)
+        os.remove(pdf_file)
+
+        doc.make(cur_dir, 'test')
+        test_tex_file()
+        self.assertTrue(exists(pdf_file))
+        os.remove(tex_file)
+        os.remove(pdf_file)
         
 if __name__ == '__main__':
     unittest.main()
