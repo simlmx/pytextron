7ae7536d13b7d866359c1b14f767ecaeccdb8c66
Simon Lemieux <lemieux.simon@gmail.com>
2012-03-12 13:24:44 -0400

    added join and stack methods for blocks

--- __init__.py
+++ __init__.py
@@ -1,6 +1,10 @@
-# TODO : test for this
-def concatenate(*block_list):
-    """ Concatenate a list of Blocks. """
-    return reduce(lambda x,y : x+y, block_list)
-
 from document import *
+
+def join(*block_list):
+    """ Concatenate a list of Blocks. """
+    return u''.join(map(unicode,block_list))
+
+def stack(*block_list):
+    r""" Concatenate a list of Blocks with '\n' """
+    return '\n'.join(map(unicode,block_list))
+
