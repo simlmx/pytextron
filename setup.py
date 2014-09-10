fa3b4a28da32c4023209e4d9033e40c3563d38d3
Simon Lemieux <lemieux.simon@gmail.com>
2014-09-10 11:09:19 -0400

    reorganize in a clean python project

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='pytextron',
+      version='0.1',
+      description='latex in python',
+      author='Simon Lemieux',
+      author_email='lemieux.simon (at) gmail (dot) you know what',
+      url='',
+      packages=['pytextron'])
